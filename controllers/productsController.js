5db38296b3cda75babba554aa8b9cc1752ad4ad7
sergioromeroo <manusergio96@gmail.com>
2022-03-21 16:35:18 -0300

    productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const {} = require('../data/products_db');
+
 module.exports = {//TODO ESTO ES PARA  ME RENDERISE EL INDEX.EJS A HTML
     add : (req,res) => {
         return res.render('addproducts')
