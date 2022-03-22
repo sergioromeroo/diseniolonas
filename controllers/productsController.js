4cbc1ab345bc5fe9bf107dcb29e35ee9ebc4fa05
sergioromeroo <manusergio96@gmail.com>
2022-03-22 17:22:38 -0300

    carrusel y productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -13,5 +13,12 @@ module.exports = {//TODO ESTO ES PARA  ME RENDERISE EL INDEX.EJS A HTML
         return res.render('productAdd',{
             categorias
         })
+    },
+    detail : (req,res) => {/* busco producto con el id */
+        let producto = productos.find(producto => producto.id === +req.params.id)
+
+        return res.render('productDetail',{
+            producto
+        })
     }
 }
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {products,add} = require('../controllers/productsController')
+const {products,add,detail} = require('../controllers/productsController')
 
 
 router.get('/products', products)
 router.get('/add', add)
+router.get('/detail/:id',detail)
 
 module.exports = router;
