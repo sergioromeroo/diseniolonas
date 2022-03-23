4dcc783a77f8b99fd71d9ebd45f36e60ea570bcf
sergioromeroo <manusergio96@gmail.com>
2022-03-22 23:04:00 -0300

    una chotaaa

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,7 +6,11 @@ const categorias = require('../data/categories_db');
 module.exports = {//TODO ESTO ES PARA  ME RENDERISE EL INDEX.EJS A HTML
     products : (req,res) => {
         return res.render('products',{
-            productos
+            productos,
+            Lonas : productos.filter(producto => producto.category === "Lonas"),
+            Stickers : productos.filter(producto => producto.category === "Stickers"),
+            Carteles : productos.filter(producto => producto.category === "Carteles"),
+            Banners : productos.filter(producto => producto.category === "Banners"),
         })
     },
     add : (req,res) => {
