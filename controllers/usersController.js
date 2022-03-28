4ed533e46f6f74a02dcb2527a311644110ffb485
sergioromeroo <manusergio96@gmail.com>
2022-03-28 12:03:42 -0300

    register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,11 @@ module.exports = {//TODO ESTO ES PARA  ME RENDERISE EL INDEX.EJS A HTML
     register : (req,res) => {
         return res.render('register')
     },
+    
+    processRegister : (req, res) => {
+
+    },
+
     login : (req,res) => {
         return res.render('login')
     }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const {register,login} = require('../controllers/usersController');
+const {register,login,processRegister} = require('../controllers/usersController');
 
 router.get('/register',register)
+router.post('/register',processRegister)
 router.get('/login',login )
 
 module.exports = router;
